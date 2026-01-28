40f5b52eb0ba94a8446e6c2739b72019f8e67d30
Nurulloh <nurullohkomilov18@gmail.com>
2026-01-28 13:50:09 +0500

    r

--- js/login.js
+++ js/login.js
@@ -2,6 +2,10 @@ const loginBtn = document.getElementById("loginBtn");
 const registerBtn = document.getElementById("registerBtn");
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
+const elToast = document.getElementById("toast");
+const elToastTemplate = document.getElementById("toastTemplate");
+const elButton = document.getElementById("js-loader-button");
+const btnLoader = document.getElementById("btnLoader");
 
 loginBtn.onclick = function () {
   loginForm.style.display = "block";
@@ -25,8 +29,43 @@ loginForm.addEventListener("submit", (evt) => {
   login(result);
 });
 
+elButton.addEventListener("click", () => {
+  btnLoader.classList.remove("hidden");
+  btnLoader.classList.add("opacity-100");
+
+  setTimeout(() => {
+    btnLoader.classList.add("opacity-0");
+    setTimeout(() => {
+      btnLoader.classList.add("hidden");
+    }, 3000);
+  }, 15000);
+});
+
+elLoginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log;
+  const formData = new FormData(elLoginForm);
+  const obj = {};
+
+  formData.forEach((value, key) => {
+    obj[key] = value;
+
+    const loader = document.getElementById("loader");
+    const elapsed = Date.now() - startTime;
+    const remaining = Math.max(MIN_LOAD_TIME - elapsed, 0);
+
+    setTimeout(() => {
+      loader.classList.add("opacity-0");
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 3000);
+    }, remaining);
+  });
+  login(obj);
+});
+
 function login(data) {
-  fetch("https://json-api.uz/api/project/fn44-amaliyot/auth/login", {
+  fetch(" https://json-api.uz/api/project/fn44-amaliyot/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -38,10 +77,49 @@ function login(data) {
     })
     .then((res) => {
       localStorage.setItem("token", res.access_token);
-      location.href = "./index.html";
+      location.href = "../index.html";
     })
     .catch(() => {
-      alert("Login Yoki Parol Xato ❌");
+      alert("Login yoki parol xato");
     });
 }
 
+// LOADER
+const MIN_LOAD_TIME = 3000;
+const startTime = Date.now();
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const elapsed = Date.now() - startTime;
+  const remaining = Math.max(MIN_LOAD_TIME - elapsed, 0);
+
+  setTimeout(() => {
+    loader.classList.add("opacity-0"); // fade out
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 3000);
+  }, remaining);
+});
+
+// toost
+elLoginForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(elLoginForm);
+  const obj = {};
+
+  formData.forEach((value, key) => {
+    obj[key] = value;
+  });
+  for (let key in obj) {
+    if (obj[key] == "") {
+      const clone = elToastTemplate.cloneNode(true).content;
+      clone.querySelector("span").innerText = `${key} joyni to'ldring`;
+      elToast.appendChild(clone);
+
+      setTimeout(() => {
+        document.querySelector(`[role="alert"]`).remove();
+      }, 1000);
+    }
+  }
+  add(obj);
+});
--- js/registatsa.js
+++ js/registatsa.js
@@ -2,6 +2,10 @@ const loginBtn = document.getElementById("loginBtn");
 const registerBtn = document.getElementById("registerBtn");
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
+const elToast = document.getElementById("toast");
+const elToastTemplate = document.getElementById("toastTemplate");
+const elButton = document.getElementById("js-loader-button");
+const btnLoader = document.getElementById("btnLoader");
 
 loginBtn.onclick = function () {
   loginForm.style.display = "block";
@@ -25,6 +29,67 @@ loginForm.addEventListener("submit", (evt) => {
   login(result);
 });
 
+// loader button
+elButton.addEventListener("click", () => {
+  const formData = new FormData(elLoginForm);
+  let hasEmpty = false;
+
+  formData.forEach((value) => {
+    if (value.trim() === "") {
+      hasEmpty = true;
+    }
+  });
+  if (hasEmpty) {
+    showToast();
+    return;
+  }
+     btnLoader.classList.remove("hidden");
+   btnLoader.classList.add("opacity-100");
+
+  elLoginForm.requestSubmit();
+
+     setTimeout(() => {
+       btnLoader.classList.add("opacity-0");
+       setTimeout(() => {
+         btnLoader.classList.add("hidden");
+       }, 3000);
+    }, 15000);
+});
+elLoginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(elLoginForm);
+  const obj = {};
+
+  formData.forEach((value, key) => {
+    obj[key] = value;
+  });
+
+  login(obj);
+});
+elLoginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log;
+  const formData = new FormData(elLoginForm);
+  const obj = {};
+
+  formData.forEach((value, key) => {
+    obj[key] = value;
+
+    const loader = document.getElementById("loader");
+    const elapsed = Date.now() - startTime;
+    const remaining = Math.max(MIN_LOAD_TIME - elapsed, 0);
+
+    setTimeout(() => {
+      loader.classList.add("opacity-0"); // fade out
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 3000);
+    }, remaining);
+  });
+  login(obj);
+ });
+
 function login(data) {
   fetch("https://json-api.uz/api/project/fn44-amaliyot/auth/register", {
     method: "POST",
@@ -38,8 +103,52 @@ function login(data) {
     })
     .then((res) => {
       localStorage.setItem("token", res.access_token);
-      location.href = "./index.html";
+      location.href = "../index.html";
     })
-    .catch(() => {});
+    .catch(() => {
+      alert("Xatolik yuz berdi, qayta urinib ko'ring");
+    });
 }
 
+// LOADER
+const MIN_LOAD_TIME = 3000;
+const startTime = Date.now();
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const elapsed = Date.now() - startTime;
+  const remaining = Math.max(MIN_LOAD_TIME - elapsed, 0);
+
+  setTimeout(() => {
+    loader.classList.add("opacity-0"); // fade out
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 3000);
+  }, remaining);
+});
+
+// toost qismi
+
+function showToast() {
+  elLoginForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const formData = new FormData(elLoginForm);
+    const obj = {};
+
+    formData.forEach((value, key) => {
+      obj[key] = value;
+    });
+    for (let key in obj) {
+      if (obj[key] == "") {
+        const clone = elToastTemplate.cloneNode(true).content;
+        clone.querySelector("span").innerText = `${key} joyni to'ldring`;
+        elToast.appendChild(clone);
+
+        setTimeout(() => {
+          document.querySelector(`[role="alert"]`).remove();
+        }, 1000);
+      }
+    }
+    add(obj);
+  });
+}
